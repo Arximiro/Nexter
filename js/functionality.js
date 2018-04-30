7974935f1690d4c722452e81b51407008c112fd0
David Ridgley <Arximiro@outlook.com>
2018-04-30 13:15:20 -0400

    navigation sidebar added and styled

---
+++ js/functionality.js
@@ -0,0 +1,19 @@
+document.getElementById("navigation__about").onclick = () => {
+  document.getElementById("navi-toggle").checked = false;
+};
+
+document.getElementById("navigation__features").onclick = () => {
+  document.getElementById("navi-toggle").checked = false;
+};
+
+document.getElementById("navigation__story").onclick = () => {
+  document.getElementById("navi-toggle").checked = false;
+};
+
+document.getElementById("navigation__homes").onclick = () => {
+  document.getElementById("navi-toggle").checked = false;
+};
+
+document.getElementById("navigation__gallery").onclick = () => {
+  document.getElementById("navi-toggle").checked = false;
+};
